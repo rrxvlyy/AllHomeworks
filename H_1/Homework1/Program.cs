c10c0b320ebda7c59f016957198d79739720289b
rrxvlyy <ibr.emil1805@yandex.ru>
2025-12-04 14:30:50 +0300

    First Dz
    First Dz

---
+++ H_1/Homework1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 0 задание
+            //Console.WriteLine("Hello, World!");
+
+            // 1 задание
+            Console.Write("Введите первое число: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сумма чисел: {number1}");
+
+
+        }
+    }
+}
