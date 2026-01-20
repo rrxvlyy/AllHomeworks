b91e1ace6788ed4229cc8b85d1e760c8e6396f23
rrxvlyy <ibr.emil1805@yandex.ru>
2026-01-20 11:46:13 +0300

    Homework 5 Level 1

---
+++ Homework 5 Level 1/Level1/Heal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Level1
+{
+    internal class Heal : Spell
+    {
+        private int healAmount;
+
+        public Heal() : base("Лечение", "Восстанавливает 10-20 здоровья")
+        {
+            Random rand = new Random();
+            healAmount = rand.Next(10, 21);
+        }
+
+        public override void Cast(Character caster, Character target)
+        {
+            Console.WriteLine($"{caster.Name} применяет лечение на {target.Name}");
+            target.Heal(healAmount);
+        }
+    }
+}
---
+++ Homework 5 Level 1/Level1/Shield.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level1
+{
+    internal class Shield : Spell
+    {
+        private int shieldAmount;
+
+        public Shield() : base("Щит", "Даёт временную защиту на 5-10 урона")
+        {
+            Random rand = new Random();
+            shieldAmount = rand.Next(5, 11);
+        }
+
+        public override void Cast(Character caster, Character target)
+        {
+            Console.WriteLine($"{caster.Name} накладывает щит на {target.Name}");
+            Console.WriteLine($"{target.Name} получает временную защиту на {shieldAmount} урона");
+            target.Heal(shieldAmount / 2);
+        }
+    }
+}
