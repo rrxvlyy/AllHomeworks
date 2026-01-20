88f04938ae00e47380bc619c9cd10ed3737f923d
rrxvlyy <ibr.emil1805@yandex.ru>
2026-01-20 12:01:11 +0300

    Homework 5 Level 1

---
+++ H_8/Level1/Fireball.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level1
+{
+    internal class Fireball : Spell
+    {
+        private int damage;
+
+        public Fireball() : base("Огненный шар", "Наносит 15-25 урона огнём")
+        {
+            Random rand = new Random();
+            damage = rand.Next(15, 26);
+        }
+
+        public override void Cast(Character caster, Character target)
+        {
+            Console.WriteLine($"{caster.Name} бросает огненный шар в {target.Name}");
+            target.TakeDamage(damage);
+        }
+    }
+}
---
+++ H_8/Level1/Mage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level1
+{
+    internal class Mage : Character
+    {
+        private List<Spell> spellBook;
+
+        public Mage(string name) : base(name, 100)
+        {
+            spellBook = new List<Spell>();
+        }
+
+        public void AddSpell(Spell spell)
+        {
+            spellBook.Add(spell);
+            Console.WriteLine($"{Name} получил заклинание: {spell.Name}");
+        }
+
+        public void ShowSpellBook()
+        {
+            Console.WriteLine($"Книга заклинаний {Name}:");
+            for (int i = 0; i < spellBook.Count; i++)
+            {
+                Console.Write($"{i + 1}. ");
+                spellBook[i].ShowInfo();
+            }
+        }
+
+        public void CastSpell(int spellIndex, Character target)
+        {
+            if (is_Alive == false)
+            {
+                Console.WriteLine($"{Name} умер и не может получать заклинания");
+                return;
+            }
+
+            if (spellIndex < 1 || spellIndex > spellBook.Count)
+            {
+                Console.WriteLine("Неверный номер заклинания");
+                return;
+            }
+
+            Spell spell = spellBook[spellIndex - 1];
+            Console.WriteLine($"\n{Name} применяет {spell.Name} на {target.Name}");
+            spell.Cast(this, target);
+        }
+
+        public int SpellsCount => spellBook.Count;
+    }
+}
